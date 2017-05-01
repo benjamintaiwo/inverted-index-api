5b1d554f4521706eae9645266a3cc73ad881f05c
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-01 16:42:43 +0100

    Add spec for Inverted-Index class

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,48 +1,18 @@
-const index = new index(readData); 
-describe('Read book data', () => {
-  it('Ensures first that file is a valid JSON file', () => {
-    expect(readData.isValidJson(JSON.stringify(file1))).toBe(true);
-    expect(readData.isValidJson(file1)).toBe('Invalid Json file');
-    });
+const books = require('fixture/books');
 
-    it('Ensures the file content is actually a valid JSON Array', () => {
-      index.uploadedFiles['file1.json'] = file1;
-      expect(readData.checkJson('file1.json', index.uploadedFiles)).toBe(true);
-    });
+// A test suite to read book data
+describe('Inverted Index Suite', () => {
+  //Create an instance of the Index class
+  const newIndex = new InvertedIndex();
+  const emptyBook = [];
+  const sampleSentence = 'Tenses are %correct but define each #well';
+  const multipleSearch = 'Let fear and doubt dissolve quickly';
+  newIndex.createIndex('books', books);
 
-    it('Ensures JSON array is not empty', () => {
-      index.uploadedFiles['file1.json'] = file1;
-      index.uploadedFiles['file2.json'] = file2;
-      expect(readData.checkJson('file1.json', index.uploadedFiles)).toBe(true);
-      expect(readData.checkJson('file2.json', index.uploadedFiles)).toBe(false);
-    });
-
-    describe('Get Index', () => {
-    index.uploadedFiles['file1.json'] = file1;
-    index.createIndex('file1.json');
-    index.getIndex('file1.json');
-
-    it('Ensures ​getIndex​ method takes a string argument that specifies the location of the JSON data.', () => {
-      expect(index.getIndex('file1.json')).toEqual(jasmine.any(Object));
-    });
-  });
-
-  describe('Populate Index', () => {
-    index.uploadedFiles['file1.json'] = file1;
-    const creator = index.createIndex('file1.json');
-    const indexed = index.getIndex('file1.json');
-
-    it('Ensures that the index is created once the JSON file has been read', () => {
-      expect(indexed['file1.json'].an).toEqual([0]);
-      expect(indexed['file1.json'].a).toEqual([0, 1]);
-    });
-
-    it('Ensures that the index returned is an object', () => {
-      expect(creator).toBe(true);
-      expect(typeof indexed).toBe('object');
-    });
-
-    it('Ensures index is not overwritten by a new JSON file', () => {
-      expect(Object.keys(index.indexMap)).toEqual(['file1.json', 'file2.json']);
+  describe('Class Inverted Index', () => {
+    it('should be a class', () => {
+      expect(newIndex instanceof InvertedIndex).toBe(true);
+      expect(newIndex instanceof Object).toBe(true);
+      expect(typeof (newIndex)).toBe('object');
     });
   });
