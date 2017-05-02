d703b633bb0a22916019725cf765fbb19ab2a6d5
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-02 19:31:37 +0100

    Add tokenize method to class constructor

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,9 +1,32 @@
+/**
+ * Inverted index class
+ */
 class InvertedIndex {
-
   /**
-   * Creates an instance of InvertedIndex.
-   * @memberOf InvertedIndex
+   * Inverted index constructor
    */
   constructor() {
+    // Object to hold the index
     this.index = {};
   }
+
+
+  static tokenize(words) {
+    return words.trim().replace(/-/g, ' ')
+      .replace(/[.,\/#!$%\^&@\*;:'{}=\_`~()]/g, '')
+      .toLowerCase()
+      .split(' ')
+      .sort();
+  }
+
+  /**
+   * @param{String} words - The string to be filtered
+   * @return{Array} tokens - Without duplicated words
+   */
+  static uniqueWords(words) {
+    const tokens = InvertedIndex.tokenize(words);
+    return tokens.filter((item, index) => tokens.indexOf(item) === index);
+  }
+
+
+  
