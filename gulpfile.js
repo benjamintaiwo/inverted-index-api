9bf2d929020ca4472d5ce57fea4c8db2b26d4ee9
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-05 09:35:00 +0100

    Add guulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,48 @@
-const gulp = require('gulp');
+/*  eslint linebreak-style: ["error", "windows"]*/
+ // const gulp = require('gulp');
+
+// import gulp from 'gulp';
+const babel = require('gulp-babel');
+
 const jasmine = require('gulp-jasmine');
+
+const gulp = require('gulp');
+
+
+// const jasmine = require('gulp-jasmine');
+/* const nodemon = require('gulp-nodemon');
+const istanbul = require('gulp-istanbul');
+const coveralls = require('gulp-coveralls');
+ const babel = require('gulp-babel');*/
+
 
 gulp.task('run-tests', () => {
   gulp.src('tests/inverted-index-test.js')
+  .pipe(babel())
   .pipe(jasmine());
 });
 
-//gulp.task('default', [ 'run-tests' ]);
+/*gulp.task('serve', () => {
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    env: { 'NODE_ENV': 'development' }
+  });
+});
+
+gulp.task('coverage', () => {
+  return gulp.src('src/inverted-index.js')
+    .pipe(istanbul({includeUntested: true}))
+    .on('finish', () => {
+      gulp.src('tests/inverted-index-test.js')
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports({
+          dir: 'coverage-report',
+          reporters: ['lcov'],
+          reportOpts: { dir: 'coverage-report'}
+        }));
+    });
+});*/
+
+gulp.task('default', [ 'run-tests' ]);
 
