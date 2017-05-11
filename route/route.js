f6603aadc012883f56e7cc7ffa30de4c4a82db79
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-11 16:02:40 +0100

    Fix search route

--- route/route.js
+++ route/route.js
@@ -11,6 +11,7 @@ const search = multer();       // This is used for search endpoint since no uplo
 const router = express.Router();
 const newIndex = new InvertedIndex();
 
+
 /**
  * @return {Object} content of file in file
  * @param {String} fileName Name of file to be read
@@ -39,22 +40,25 @@ router.post('/api/create', (req, res) => {
       } else {
         const filePath = (req.file.filename).toString();
         newIndex.createIndex(req.file.originalname, readFile(filePath));
-        res.json(newIndex.getIndex(req.file.originalname));
+        res.json(newIndex.getIndex());
       }
-      fs.unlinkSync(path.join('fixtures', req.file.filename)); // delete the uploaded file once the index is created
+      fs.unlinkSync(path.join('fixtures', req.file.filename));
+      // delete the uploaded file once the index is created
     }
   });
 });
 // search endpoint
-router.post('/api/search', (req, res) => {
-  if (req.file.originalname !== undefined) {
-    if (req.body.searchQuery.length === 0) {
-      res.json({ error: 'The searchTerms cannot be empty' });
-    } else {
-      newIndex.getIndex(req.file.originalname);
-      res.json(newIndex.searchIndex(req.file.originalname, req.body.searchQuery, newIndex.index));
-    }
-  }
+router.post('/api/search', search.single(), (req, res) => {
+  // const filePath = (req.file.filename).toString();
+  // newIndex.createIndex(req.file.originalname, readFile(filePath));
+  // newIndex.getIndex(req.file.originalname);
+  // if (this.index(req.file.originalname) !== undefined) {
+  res.json(newIndex.searchIndex(req.body.fileName, req.body.searchQuery));
 });
+  /* const searchQuery = `${req.body.searchQuery}`;
+  const searchResult = newIndex.searchIndex(searchQuery);
+  res.json(searchResult);*/
+
+ //  res.json(newIndex.searchIndex(req.body.searchQuery, newIndex.index));
 
 export default router;
